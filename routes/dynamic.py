a7e03a14e0841f18c7c642ee99dcb3bc89425c0e
abduselam <abduselamm555@gmail.com>
2026-03-24 23:32:48 +0300

    fixed date issue

--- routes/dynamic.py
+++ routes/dynamic.py
@@ -81,6 +81,14 @@ def parse_extended_json(data: Any) -> Any:
     
     return data
 
+@router.get("/collections/", response_description="List all collections", summary="List collections", response_model=List[str])
+async def list_collections():
+    """
+    Retrieve a list of all existing collection names in the current database.
+    """
+    collections = await db.list_collection_names()
+    return collections
+
 @router.post("/{collection_name}/", response_description="Add new document(s)", summary="Create document(s) dynamically", response_model=Union[Dict[str, Any], List[Dict[str, Any]]])
 async def create_document(collection_name: str, document: Union[Dict[str, Any], List[Dict[str, Any]]] = Body(...)):
     """
